8312c009b910ee3155b17bddb798d4e37266531f
md-redwan-hossain <redwan.ping@gmail.com>
2025-09-12 17:24:47 +0600

    folder restructure

---
+++ Tests/SharpPersistence.ReflectionTests/SqlParserTest.cs
@@ -0,0 +1,16 @@
+﻿using SharpPersistence.Abstractions;
+using SharpPersistence.Parsers;
+using Shouldly;
+
+namespace SharpPersistence.ReflectionTests;
+
+public class SqlParserTest
+{
+    private readonly IParsedSqlStorage _parsedSqlStorage = new SqlParser().ParseFromCallingAssembly();
+
+    [Fact]
+    public void ParseFromStorage_Should_Not_Be_Empty()
+    {
+        _parsedSqlStorage.ShouldNotBeEmpty();
+    }
+}
