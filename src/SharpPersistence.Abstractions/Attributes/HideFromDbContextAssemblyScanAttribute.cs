2ebacd0cdc8eedc69015c0a9149ff1692b96bd97
md-redwan-hossain <redwan.ping@gmail.com>
2026-06-03 16:55:49 +0600

    moved to central pkg management

---
+++ src/SharpPersistence.Abstractions/Attributes/HideFromDbContextAssemblyScanAttribute.cs
@@ -0,0 +1,4 @@
+﻿namespace SharpPersistence.Abstractions.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+public sealed class HideFromDbContextAssemblyScanAttribute : Attribute;
